ac0e4b9bda12b77c57ff7e838507727935ad4902
AllRounderGamin <78315461+AllRounderGamin@users.noreply.github.com>
2022-11-09 21:54:14 +0000

    Basic Dragging Functionality

--- App/InteractiveNode.js
+++ App/InteractiveNode.js
@@ -1,6 +1,6 @@
 const NODE = document.createElement("template");
 NODE.innerHTML = '<link href="./stylesheet.css" rel="stylesheet"> \n' +
-    '<div class = "node">\n' +
+    '<div class = "node" draggable="true">\n' +
     '   <p class="nodeName" contenteditable="true"></p> \n' +
     '</div>'
 
@@ -11,9 +11,29 @@ class InteractiveNode extends HTMLElement{
         this.initialise();
     }
 
-    initialise(){
+    initialise() {
         this.shadow = this.attachShadow({mode: "open"});
         this.shadow.appendChild(NODE.content.cloneNode(true));
+        this.addEventListener("dragstart", this.dragStart);
+        this.addEventListener("dragover", this.dragEvent);
+        this.addEventListener("dragend", this.dragEnd);
+    }
+
+    dragStart(e){
+        const el = document.createElement("interactive-node");
+        el.id = "FakeNode";
+        el.hidden = true;
+        document.querySelector("body").appendChild(el);
+        e.dataTransfer.setDragImage(el, 0, 0);
+    }
+
+    dragEvent(e){
+        this.style.setProperty("--Xpos", e.pageX - 50 + "px");
+        this.style.setProperty("--Ypos", e.pageY - 50 + "px");
+    }
+
+    dragEnd(){
+        document.querySelector("body").removeChild(document.querySelector("#FakeNode"));
     }
 
     // event listeners for dragging and connecting(?)
@@ -21,3 +41,73 @@ class InteractiveNode extends HTMLElement{
 }
 
 customElements.define("interactive-node", InteractiveNode);
+
+
+
+/*
+        this.shadow.addEventListener("dragstart", this.dragStart);
+        this.shadow.addEventListener("dragend", this.dragEnd);
+
+dragStart(e){
+    const fakeNode = document.createElement("interactive-node");
+    fakeNode.style.setProperty("--Xpos", -1000 + "px");
+    fakeNode.style.setProperty("--Ypos", -1000 + "px");
+    fakeNode.shadowRoot.querySelector(".nodeName").textContent = e.target.querySelector(".nodeName").textContent
+    document.querySelector("body").appendChild(fakeNode);
+    e.dataTransfer.setDragImage(fakeNode, 0, 0);
+    e.target.hidden = true;
+}
+
+dragEnd(e){
+    e.target.hidden = false;
+}
+
+
+
+
+
+
+dragStart(e){
+        const el = document.createElement("interactive-node");
+        el.hidden = true;
+        document.querySelector("body").appendChild(el);
+        e.dataTransfer.setDragImage(el, 0, 0);
+    }
+
+    dragEvent(e){
+        this.style.setProperty("--Xpos", e.pageX - 50 + "px");
+        this.style.setProperty("--Ypos", e.pageY - 50 + "px");
+        console.log("test");
+    }
+
+
+
+
+
+
+
+
+            this.addEventListener("dragstart", this.dragStart);
+        this.addEventListener("mousedown", this.mouseDown);
+    }
+
+    dragStart(e){
+        e.preventDefault();
+        return false;
+    }
+
+    mouseDown(e){
+        document.addEventListener("mousemove", this.mouseMove);
+        document.addEventListener("mouseup", this.mouseUp);
+    }
+
+    mouseMove(e){
+        e.target.style.setProperty("--Xpos", e.pageX - 50 + "px");
+        e.target.style.setProperty("--Ypos", e.pageY - 50 + "px");
+    }
+
+    mouseUp(e){
+        e.target.removeEventListener("mousemove", this.mouseMove);
+        e.target.removeEventListener("mouseup", this.mouseUp);
+    }
+*/
